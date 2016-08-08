ed30e2cc83265229af68af54a6c01add69278745
no7dw <no7david@gmail.com>
2016-08-08 10:38:36 +0800

    schedule work

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 'use strict'
 
+var schedule = require('node-schedule');
 const rp = require('request-promise')
 const clientPort = 8001
 
-var options = {
-    method: 'POST',
-    uri: 'http://localhost:'+ clientPort + '/r',
-    body: {
-        argv: 2000
-    },
-    json: true
-};
-rp(options)
-    .then(function (body) {
-        // POST succeeded... 
-        console.log("client: %d body: %s", clientPort , JSON.stringify(body))
-    })
-    .catch(function (err) {
-        // POST failed... 
-        console.log("failed", err)
-    });
+// @path example: '/path'
+// @body json
+var runClient = function(path, method ,body) {
+    var options = {
+        method: method,
+        uri: 'http://localhost:'+ clientPort +  path,
+        json: true
+    };
+    rp(options)
+        .then(function (body) {
+            console.log("client: %d body: %s", clientPort , JSON.stringify(body))
+        })
+        .catch(function (err) {
+            console.log("failed", err)
+        });
+    }
+runClient('/r', 'POST' , {argv:3000})
 
+var rule = new schedule.RecurrenceRule()
+rule.second = 1
+schedule.scheduleJob(rule,function(){
+  runClient('/h', 'GET' , {})
+  //console.log('The answer to life, the universe, and everything!', new Date());
+})
+
+runClient('/h', 'GET' , {})
